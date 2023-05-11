102f8c02d077df6195566c4b2ee06a3864e835d0
Egyed József <jegyed50@gmail.com>
2023-05-11 09:18:44 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,23 @@
-//  https://github.com/jegyed50/jegyed50-bbc-microbit-study365-blocks-03-04-a
-//  JEGYED50-BBC-MICROBIT-STUDY365-BLOCKS-03-04-B
-//  POMODORO TIMER
-//  Betűk W25R5W25R5W25R5W25R15 25p=2,5s 5p =0,s,15p=1,5s
-basic.forever(function on_forever() {
-    
+input.onButtonPressed(Button.A, function () {
+    basic.showString("w")
+    basic.pause(2500)
+    basic.showString("R")
+    basic.pause(500)
+    basic.showString("w")
+    basic.pause(2500)
+    basic.showString("R")
+    basic.pause(500)
+    basic.showString("w")
+    basic.pause(2500)
+    basic.showString("w")
+    basic.showString("R")
+    basic.pause(1500)
+    basic.clearScreen()
+})
+// https://github.com/jegyed50/jegyed50-bbc-microbit-study365-blocks-03-04-a
+// JEGYED50-BBC-MICROBIT-STUDY365-BLOCKS-03-04-B
+// POMODORO TIMER
+// Betűk W25R5W25R5W25R5W25R15 25p=2500ms, 5p =500ms, 15p=1500ms
+basic.forever(function () {
+	
 })
