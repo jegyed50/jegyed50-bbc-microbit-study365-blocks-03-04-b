1c2095cd234d529f42b1240a1f4a49307bc4e79b
Egyed József <jegyed50@gmail.com>
2023-05-11 09:09:32 +0200

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,8 @@
+# https://github.com/jegyed50/jegyed50-bbc-microbit-study365-blocks-03-04-a
+# JEGYED50-BBC-MICROBIT-STUDY365-BLOCKS-03-04-B
+# POMODORO TIMER
+# Betűk W25R5W25R5W25R5W25R15 25p=2,5s 5p =0,s,15p=1,5s
+
+def on_forever():
+    pass
+basic.forever(on_forever)
---
+++ main.ts
@@ -0,0 +1,7 @@
+//  https://github.com/jegyed50/jegyed50-bbc-microbit-study365-blocks-03-04-a
+//  JEGYED50-BBC-MICROBIT-STUDY365-BLOCKS-03-04-B
+//  POMODORO TIMER
+//  Betűk W25R5W25R5W25R5W25R15 25p=2,5s 5p =0,s,15p=1,5s
+basic.forever(function on_forever() {
+    
+})
