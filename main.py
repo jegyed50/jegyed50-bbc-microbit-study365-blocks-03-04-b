5cd9065956cec7702789b82f45d199ee82cd5ccc
Egyed József <jegyed50@gmail.com>
2023-05-11 10:11:22 +0200

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
+def on_button_pressed_a():
+    basic.show_string("w")
+    basic.pause(2500)
+    basic.show_string("R")
+    basic.pause(500)
+    basic.show_string("w")
+    basic.pause(2500)
+    basic.show_string("R")
+    basic.pause(500)
+    basic.show_string("w")
+    basic.pause(2500)
+    basic.show_string("w")
+    basic.show_string("R")
+    basic.pause(1500)
+    basic.clear_screen()
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
 # https://github.com/jegyed50/jegyed50-bbc-microbit-study365-blocks-03-04-a
 # JEGYED50-BBC-MICROBIT-STUDY365-BLOCKS-03-04-B
 # POMODORO TIMER
-# Betűk W25R5W25R5W25R5W25R15 25p=2,5s 5p =0,s,15p=1,5s
+# Betűk W25R5W25R5W25R5W25R15 25p=2500ms, 5p =500ms, 15p=1500ms
 
 def on_forever():
     pass
